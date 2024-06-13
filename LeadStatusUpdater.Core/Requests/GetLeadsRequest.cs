959087dcadb331a58a22e16dbb6201a34b5e4e30
xnesart <xnesart@mail.ru>
2024-06-13 13:46:11 +0300

    request and response is changed

--- LeadStatusUpdater.Core/Requests/GetLeadsRequest.cs
+++ LeadStatusUpdater.Core/Requests/GetLeadsRequest.cs
@@ -6,4 +6,7 @@ public class GetLeadsRequest
 {
     public List<LeadDto> Leads { get; set; }
     public int TimePeriodInDays { get; set; }
+    public int CountOfTransactions { get; set; }
+    public int TimePeriodForCalculatingChange { get; set; }
+    public decimal Change { get; set; }
 }
--- LeadStatusUpdater.Core/Responses/GetLeadsResponse.cs
+++ LeadStatusUpdater.Core/Responses/GetLeadsResponse.cs
@@ -6,4 +6,7 @@ public class GetLeadsResponse
 {
     public List<LeadDto> Leads { get; set; }
     public int TimePeriodInDays { get; set; }
+    public int CountOfTransactions { get; set; }
+    public int TimePeriodForCalculatingChange { get; set; }
+    public decimal Change { get; set; }
 }
